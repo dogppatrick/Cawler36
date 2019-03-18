3fc2a1b76b48a2cd245cb9f91ca277c36a4adc3c
dogppatrick@msn.com <dogppatrick@msn.com>
2019-03-18 09:44:57 +0800

    update

--- ptt_c/Gossip_title_url.py
+++ ptt_c/Gossip_title_url.py
@@ -4,12 +4,13 @@ import warnings
 import requests
 import pandas as pd
 warnings.filterwarnings("ignore")
-page = 1
+start_page = 1
+page = start_page
+l_push = 50
 b = False
 df = pd.DataFrame(columns=["title", "author", "data", "url"])
 while True:
-    start_url = "https://www.ptt.cc/bbs/Gossiping/search?page=" + str(page) + "&q=recommend%3A40"
-    #                                                             頁數    推文數量>40
+    start_url = "https://www.ptt.cc/bbs/Gossiping/search?page=" + str(page) + "&q=recommend%3A" + str(l_push)
     url = start_url
     print(url)
     # r = Request(url)
@@ -21,20 +22,24 @@ while True:
     all_art = main_con.find("div", class_="r-list-container")
     art_box = all_art.find_all("div", class_="r-ent")
     for box in art_box:
-        a_author = box.find("div", class_="meta").find("div", class_="author").text
-        a_date = box.find("div", class_="meta").find("div", class_="date").text
-        if a_date == " 3/12":
-            b = True
-        if a_author not in "-":
-            a_title = box.find("div", class_="title").text.strip()
-            a_url = box.find("div", class_="title").find("a")["href"]
-            a_url = "https://www.ptt.cc" + a_url
-            data = a_title, a_author, a_date, a_url
-            s = pd.Series(data, index=["title", "author", "data", "url"])
-            df = df.append(s, ignore_index="true")
+        try:
+            a_author = box.find("div", class_="meta").find("div", class_="author").text
+            a_date = box.find("div", class_="meta").find("div", class_="date").text
+            if a_date == "11/11" or page-start_page >= 1000:
+                b = True
+            if a_author not in "-":
+                a_title = box.find("div", class_="title").text.strip()
+                a_url = box.find("div", class_="title").find("a")["href"]
+                a_url = "https://www.ptt.cc" + a_url
+                data = a_title, a_author, a_date, a_url
+                s = pd.Series(data, index=["title", "author", "data", "url"])
+                df = df.append(s, ignore_index="true")
+        except TypeError:
+            print("error find")
+            print(box.find("div", class_="title").text)
     page = page + 1
     if b:
         break
 # print(articles[0])
 # print(articles[0].text)
-df.to_csv("gossip_title.csv", encoding="utf-8", index=False)
+df.to_csv("gossip_50p_0317_03.csv", encoding="utf-8", index=False)
--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-a = {"a1": "aa", "a2": "bb"}
-print(a)
-print("a1"in a)
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+import warnings
+import requests
+import pandas as pd
+import math
+warnings.filterwarnings("ignore")
+
+
+url = "https://www.ptt.cc/bbs/Gossiping/M.1552728565.A.639.html"
+print(url)
+response = requests.get(url, cookies={"over18": "1"})
+html = BeautifulSoup(response.text)
